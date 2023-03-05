926f56b09b5dc411219f6b1277c1f792ec4eb563
Nancy Chaudhry <nancych14@gmail.com>
2023-03-05 16:25:05 +0500

    First commit

---
+++ js/script.js
@@ -0,0 +1,62 @@
+// For temperature in Fahrenheit use units=imperial
+// For temperature in Celsius use units=metric
+const token = config.API_KEY;
+const btn = document.querySelector('button');
+const cityInput = document.querySelector('#city');
+const main = document.querySelector('main');
+const unitSelect = document.querySelector('.unit');
+
+async function getWeather() {
+  try {
+    city = cityInput.value;
+    units = unitSelected();
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${token}&units=${units}`)
+
+    const cityData = await response.json();
+    displayWeatherData(cityData);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function displayWeatherData(cityData) {
+  cityInput.value = "";
+  main.innerText = "";
+  const cardDiv = document.createElement("div");
+  cardDiv.classList.add("card");
+  const cityDate = new Date(cityData.dt * 1000);
+  const datePara = document.createElement('p');
+  datePara.innerText = cityDate.toUTCString();
+  datePara.classList.add('date');
+  cardDiv.appendChild(datePara);
+	const cityName = document.createElement("h3");
+  cityName.classList.add('name');
+	cityName.textContent = `${cityData.name}, ${cityData.sys.country}`
+	cardDiv.appendChild(cityName);
+  const icon = document.createElement('img');
+  icon.classList.add('icon');
+  icon.src = `https://openweathermap.org/img/wn/${cityData.weather[0].icon}@2x.png`;
+  cardDiv.appendChild(icon);
+  const temp = document.createElement('span');
+  temp.classList.add('temp');
+  temp.innerText = `${Math.round(cityData.main.temp)}℉`;
+  const para = document.createElement('p');
+  para.classList.add('description');
+  para.innerText = `Feels like ${Math.round(cityData.main.feels_like)}℉. ${cityData.weather[0].main}. ${cityData.weather[0].description}`;
+  cardDiv.appendChild(temp);
+  cardDiv.appendChild(para);
+  main.appendChild(cardDiv);
+}
+
+function unitSelected() {
+  if (unitSelect.checked) {
+    units = 'imperial'
+  } else {
+    units = 'metric'
+  }
+  return units
+};
+
+// Event Listeners
+btn.addEventListener('click', getWeather);
+unitSelect.addEventListener('click', unitSelected);
